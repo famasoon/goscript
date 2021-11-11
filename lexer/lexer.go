1e8a388c86fa210ce463f9f64be8d682ad743237
famasoon <famasoon@gmail.com>
2021-11-11 22:44:18 +0900

    Add func NextToken

---
+++ lexer/lexer.go
@@ -0,0 +1,61 @@
+package lexer
+
+import (
+	"goscript/token"
+)
+
+type Lexer struct {
+	input       string
+	position    int
+	readPostion int
+	ch          byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPostion >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPostion]
+	}
+	l.position = l.readPostion
+	l.readPostion += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
