efa7f815ca881d2488ff0f5f7008be17fb845ca7
famasoon <famasoon@gmail.com>
2021-11-15 22:29:50 +0900

    テストケースの拡充

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,22 +6,102 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x + y;
+	};
+	let result = add(five, ten);
+	!-/*5;
+	5 < 10 > 5;
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
+	10 == 10;
+	10 != 9;
+	`
 
 	tests := []struct {
 		testName        string
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
-		{"= test case", token.ASSIGN, "="},
-		{"+ test case", token.PLUS, "+"},
-		{"( test case", token.LPAREN, "("},
-		{") test case", token.RPAREN, ")"},
-		{"{ test case", token.LBRACE, "{"},
-		{"} test case", token.RBRACE, "}"},
-		{", test case", token.COMMA, ","},
-		{"; test case", token.SEMMICOLON, ";"},
-		{"EOF test case", token.EOF, ""},
+		{"Let test case", token.LET, "let"},
+		{"IDENT test case", token.IDENT, "five"},
+		{"ASSIGN test case", token.ASSIGN, "="},
+		{"INT test case", token.INT, "5"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"Let test case", token.LET, "let"},
+		{"IDENT test case", token.IDENT, "ten"},
+		{"ASSIGN test case", token.ASSIGN, "="},
+		{"INT test case", token.INT, "10"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"LET test case", token.LET, "let"},
+		{"IDENT test case", token.IDENT, "add"},
+		{"ASSIGN test case", token.ASSIGN, "="},
+		{"FUNCTION test case", token.FUNCTION, "fn"},
+		{"LPAREN test case", token.LPAREN, "("},
+		{"IDENT test case", token.IDENT, "x"},
+		{"COMMA test case", token.COMMA, ","},
+		{"IDENT test case", token.IDENT, "y"},
+		{"RPAREN test case", token.RPAREN, ")"},
+		{"LBRACE test case", token.LBRACE, "{"},
+		{"IDENT test case", token.IDENT, "x"},
+		{"PLUS test case", token.PLUS, "+"},
+		{"IDENT test case", token.IDENT, "y"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"RBRACE test case", token.RBRACE, "}"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"Let test case", token.LET, "let"},
+		{"IDENT test case", token.IDENT, "result"},
+		{"ASSIGN test case", token.ASSIGN, "="},
+		{"IDENT test case", token.IDENT, "add"},
+		{"LPAREN test case", token.LPAREN, "("},
+		{"IDENT test case", token.IDENT, "five"},
+		{"COMMA test case", token.COMMA, ","},
+		{"IDENT test case", token.IDENT, "ten"},
+		{"RPAREN test case", token.RPAREN, ")"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"BANG test case", token.BANG, "!"},
+		{"MINUS test case", token.MINUS, "-"},
+		{"SLASH test case", token.SLASH, "/"},
+		{"ASTERISK test case", token.ASTERISK, "*"},
+		{"INT test case", token.INT, "5"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"INT test case", token.INT, "5"},
+		{"LT test case", token.LT, "<"},
+		{"INT test case", token.INT, "10"},
+		{"GT test case", token.GT, ">"},
+		{"INT test case", token.INT, "5"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"IF test case", token.IF, "if"},
+		{"LPAREN test case", token.LPAREN, "("},
+		{"INT test case", token.INT, "5"},
+		{"LT test case", token.LT, "<"},
+		{"INT test case", token.INT, "10"},
+		{"RPAREN test case", token.RPAREN, ")"},
+		{"LBRACE test case", token.LBRACE, "{"},
+		{"RETURN test case", token.RETURN, "return"},
+		{"TRUE test case", token.TRUE, "true"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"RBRACE test case", token.RBRACE, "}"},
+		{"ELSE test case", token.ELSE, "else"},
+		{"LBRACE test case", token.LBRACE, "{"},
+		{"RETURN test case", token.RETURN, "return"},
+		{"FALSE test case", token.FALSE, "false"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"RBRACE test case", token.RBRACE, "}"},
+		{"INT test case", token.INT, "10"},
+		{"EQ test case", token.EQ, "=="},
+		{"INT test case", token.INT, "10"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"INT test case", token.INT, "10"},
+		{"NOT_EQ test case", token.NOT_EQ, "!="},
+		{"INT test case", token.INT, "9"},
+		{"SEMICOLON test case", token.SEMICOLON, ";"},
+		{"Let test case", token.EOF, ""},
 	}
 
 	l := New(input)
