3ec88ac64cae9d6c72b22f8e86ba47031473c1d2
famasoon <famasoon@gmail.com>
2021-11-15 22:29:35 +0900

    キーワードの定義追記

--- token/token.go
+++ token/token.go
@@ -7,6 +7,23 @@ type Token struct {
 	Literal string
 }
 
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
