9fe5b88c7137c8552523c403362bea9c6efd545c
famasoon <famasoon@gmail.com>
2021-11-17 22:09:16 +0900

    基本的なパーサの骨組みを追加

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"goscript/ast"
+	"goscript/lexer"
+	"goscript/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
