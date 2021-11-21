0c6cece4b46e4a0292e7c4d2a458cd5ea25bff14
FAMASoon <famasoon@gmail.com>
2021-11-21 19:10:33 +0900

    式文を追加

--- ast/ast.go
+++ ast/ast.go
@@ -52,3 +52,11 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
