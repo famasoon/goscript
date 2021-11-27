3cf655007493e5a6616fdb6776921f6cded07a37
FAMASoon <famasoon@gmail.com>
2021-11-27 23:04:15 +0900

    パースする際のヘルパー関数を追加

--- parser/parser.go
+++ parser/parser.go
@@ -104,9 +104,15 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
