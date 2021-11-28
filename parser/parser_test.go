3410f2b43f8cc9150cab87a00f7274fdd3aa2da6
FAMASoon <famasoon@gmail.com>
2021-11-29 01:08:40 +0900

    中値演算子のテスト追加

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -289,3 +289,146 @@ func TestParsingInnfixExpression(t *testing.T) {
 		})
 	}
 }
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		testName string
+		input    string
+		expected string
+	}{
+		{
+			"-a * b case",
+			"-a * b",
+			"((-a) * b)",
+		},
+		{
+			"!-a case",
+			"!-a",
+			"(!(-a))",
+		},
+		{
+			"a + b + c case",
+			"a + b + c",
+			"((a + b) + c)",
+		},
+		{
+			"a + b - c case",
+			"a + b - c",
+			"((a + b) - c)",
+		},
+		{
+			"a * b * c case",
+			"a * b * c",
+			"((a * b) * c)",
+		},
+		{
+			"a * b / c case",
+			"a * b / c",
+			"((a * b) / c)",
+		},
+		{
+			"a + b / c case",
+			"a + b / c",
+			"(a + (b / c))",
+		},
+		{
+			"a + b * c + d / e - f case",
+			"a + b * c + d / e - f",
+			"(((a + (b * c)) + (d / e)) - f)",
+		},
+		{
+			"3 + 4; -5 * 5 case",
+			"3 + 4; -5 * 5",
+			"(3 + 4)((-5) * 5)",
+		},
+		{
+			"5 > 4 == 3 < 4 case",
+			"5 > 4 == 3 < 4",
+			"((5 > 4) == (3 < 4))",
+		},
+		{
+			"5 < 4 != 3 > 4 case",
+			"5 < 4 != 3 > 4",
+			"((5 < 4) != (3 > 4))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5 case",
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+		{
+			"true case",
+			"true",
+			"true",
+		},
+		{
+			"false case",
+			"false",
+			"false",
+		},
+		{
+			"3 > 5 == false case",
+			"3 > 5 == false",
+			"((3 > 5) == false)",
+		},
+		{
+			"3 < 5 == true case",
+			"3 < 5 == true",
+			"((3 < 5) == true)",
+		},
+		{
+			"1 + (2 + 3) + 4 case",
+			"1 + (2 + 3) + 4",
+			"((1 + (2 + 3)) + 4)",
+		},
+		{
+			"(5 + 5) * 2 case",
+			"(5 + 5) * 2",
+			"((5 + 5) * 2)",
+		},
+		{
+			"2 / (5 + 5) case",
+			"2 / (5 + 5)",
+			"(2 / (5 + 5))",
+		},
+		{
+			"-(5 + 5) case",
+			"-(5 + 5)",
+			"(-(5 + 5))",
+		},
+		{
+			"!(true == true) case",
+			"!(true == true)",
+			"(!(true == true))",
+		},
+		{
+			"a + add(b * c) + d case",
+			"a + add(b * c) + d",
+			"((a + add((b * c))) + d)",
+		},
+		{
+			"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8)) case",
+			"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))",
+			"add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))",
+		},
+		{
+			"add(a + b + c * d / f + g) case",
+			"add(a + b + c * d / f + g)",
+			"add((((a + b) + ((c * d) / f)) + g))",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			l := lexer.New(tt.input)
+			p := New(l)
+			program := p.ParseProgram()
+			checkParserErrors(t, p)
+
+			actual := program.String()
+			if actual != tt.expected {
+				t.Errorf("expected=%q, got=%q", tt.expected, actual)
+			}
+		})
+	}
+}
