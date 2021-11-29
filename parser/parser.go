e7d298adfa2a1cdd613726bf8c9c698d7303cc01
FAMASoon <famasoon@gmail.com>
2021-11-29 22:39:46 +0900

    真偽値リテラルを実装

--- parser/parser.go
+++ parser/parser.go
@@ -58,6 +58,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfix)
@@ -257,6 +259,10 @@ func (p *Parser) parseInfix(left ast.Expression) ast.Expression {
 	expression.Right = p.parseExpression(precedences)
 
 	return expression
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
 
 func (p *Parser) peekPrecedence() int {
