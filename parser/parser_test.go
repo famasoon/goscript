1549df5b0c6bff189645c8c548978d540c1f1a35
FAMASoon <famasoon@gmail.com>
2021-12-01 22:44:57 +0900

    関数構文のテスト追加

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -482,6 +482,57 @@ func TestIfElseExpression(t *testing.T) {
 	}
 }
 
+func TestFunctionLiteralParsing(t *testing.T) {
+	input := `fn(x,y) { x + y }`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d", 1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	function, ok := stmt.Expression.(*ast.FunctionLiteral)
+	if !ok {
+		t.Errorf("stmt.Expression is not ast.Expression. got=%T", stmt.Expression)
+	}
+
+	if len(function.Parameters) != 2 {
+		t.Fatalf("function literal parameters wrong. want 2, got=%d", len(function.Parameters))
+	}
+
+	err := testLiteralExpression(t, function.Parameters[0], "x")
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	err = testLiteralExpression(t, function.Parameters[1], "y")
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	if len(function.Body.Statements) != 1 {
+		t.Fatalf("function.Body.Statements has not 1 statements. got=%d", len(function.Body.Statements))
+	}
+
+	bodyStmt, ok := function.Body.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("function body stmt is not ast.ExpressionStatement. got=%T", function.Body.Statements[0])
+	}
+
+	err = testInfixExpression(t, bodyStmt.Expression, "x", "<", "y")
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+}
+
 /* func TestOperatorPrecedenceParsing(t *testing.T) {
 	tests := []struct {
 		testName string
