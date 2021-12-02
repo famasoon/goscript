f3a31af1f9cc8c7be9dfac67983e82bf09f2a504
FAMASoon <famasoon@gmail.com>
2021-12-02 16:42:58 +0900

    呼び出し式のテストを追加

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -568,6 +568,53 @@ func TestFunctionParameterParsing(t *testing.T) {
 	}
 }
 
+func TestCallExpressionParsing(t *testing.T) {
+	input := "add(1, 2 * 3, 4 + 5);"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d", 1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.CallExpression. got=%T", stmt.Expression)
+	}
+
+	err := testIdentifier(t, exp.Function, "add")
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	if len(exp.Arguments) != 3 {
+		t.Fatalf("wrong length of arguments. got=%d,", len(exp.Arguments))
+	}
+
+	err = testLiteralExpression(t, exp.Arguments[0], 1)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	err = testInfixExpression(t, exp.Arguments[1], 2, "*", 3)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	err = testInfixExpression(t, exp.Arguments[2], 4, "+", 5)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+}
+
 /* func TestOperatorPrecedenceParsing(t *testing.T) {
 	tests := []struct {
 		testName string
