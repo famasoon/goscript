1c031d22f7adfc01ab05529536df2298c792ec69
FAMASoon <famasoon@gmail.com>
2021-12-03 17:53:34 +0900

    *ast.Programを辿って評価するように変更

---
+++ evaluator/evaluator.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"goscript/ast"
+	"goscript/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmt []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmt {
+		result = Eval(statement)
+	}
+
+	return result
+}
