c92d774d35bf1d93237730a4e15754636c6d6e7b
FAMASoon <famasoon@gmail.com>
2021-12-03 18:46:09 +0900

    Booleanの評価テストを追加

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -48,3 +48,36 @@ func testIntegerObject(obj object.Object, expected int64) error {
 
 	return nil
 }
+
+func TestEvaBooleanExpression(t *testing.T) {
+	tests := []struct {
+		testName string
+		input    string
+		expected bool
+	}{
+		{"true case", "true", true},
+		{"false case", "false", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			evaluated := testEval(tt.input)
+			err := testBooleanObject(evaluated, tt.expected)
+			if err != nil {
+				t.Errorf("[ERROR] %v", err)
+			}
+		})
+	}
+}
+
+func testBooleanObject(obj object.Object, expected bool) error {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		return fmt.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+	}
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+	}
+
+	return nil
+}
