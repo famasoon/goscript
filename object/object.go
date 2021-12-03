abb2a3aeed2ad3037c0d527954cdaaa1a2bd1424
FAMASoon <famasoon@gmail.com>
2021-12-03 17:52:05 +0900

    fix ObjectType

--- object/object.go
+++ object/object.go
@@ -19,19 +19,19 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Type() string    { return INTEGER_OBJ }
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) Type() string    { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct {
 	Value bool
 }
 
-func (n *Null) Type() string    { return NULL_OBJ }
-func (n *Null) Inspect() string { return "null" }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
