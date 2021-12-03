94e997398574e7669e61e3bc84af9a9b5505a4e7
FAMASoon <famasoon@gmail.com>
2021-12-03 22:07:01 +0900

    True/Falseの参照を返すように変更

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"goscript/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -14,7 +19,7 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolean(node.Value)
 	}
 
 	return nil
@@ -28,4 +33,12 @@ func evalStatements(stmt []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolean(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
 }
