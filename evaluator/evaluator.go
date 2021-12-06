62c1d5c8fce74a93c8374a2e29b7890ca14e26dc
FAMASoon <famasoon@gmail.com>
2021-12-06 22:49:37 +0900

    Support boolean infix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -88,6 +88,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolean(left == right)
+	case operator == "!=":
+		return nativeBoolean(left != right)
 	default:
 		return NULL
 	}
