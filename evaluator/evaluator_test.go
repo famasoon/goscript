9717a0bd6b9031786383929c9369ab572b894d77
FAMASoon <famasoon@gmail.com>
2021-12-06 22:27:58 +0900

    Add eval boolean case

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -71,6 +71,14 @@ func TestEvaBooleanExpression(t *testing.T) {
 	}{
 		{"true case", "true", true},
 		{"false case", "false", false},
+		{"1 < 2", "1 < 2", true},
+		{"1 > 2", "1 > 2", false},
+		{"1 < 1", "1 < 1", false},
+		{"1 > 1", "1 > 1", false},
+		{"1 == 1", "1 == 1", true},
+		{"1 != 1", "1 != 1", false},
+		{"1 == 2", "1 == 2", false},
+		{"1 != 2", "1 != 2", true},
 	}
 
 	for _, tt := range tests {
