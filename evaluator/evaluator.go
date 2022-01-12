1d4813385772dbae1dfb95315ae64da50bd021b8
FAMASoon <famasoon@gmail.com>
2022-01-12 22:07:01 +0900

    [WIP] add let statement

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -56,6 +56,11 @@ func Eval(node ast.Node) object.Object {
 			return val
 		}
 		return &object.ReturnValue{Value: val}
+	case *ast.LetStatement:
+		val := Eval(node.Value)
+		if isError(val) {
+			return val
+		}
 	}
 
 	return nil
