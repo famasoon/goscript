f941fa65c7dc45c5fa9d6204004ba5c17c9d7960
FAMASoon <famasoon@gmail.com>
2022-01-24 22:31:01 +0900

    Apply eval string value

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -79,6 +79,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return applyFunction(function, args)
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	}
 
 	return nil
