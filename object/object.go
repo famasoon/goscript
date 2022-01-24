6c0bcf96f81ab50e7211c7474af064fd53696475
FAMASoon <famasoon@gmail.com>
2022-01-24 22:26:14 +0900

    Add string obj

--- object/object.go
+++ object/object.go
@@ -15,6 +15,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ       = "STRING"
 	ERROR_OBJ        = "ERROR"
 )
 
@@ -82,3 +83,10 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
