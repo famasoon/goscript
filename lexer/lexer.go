a138907da4d11c9267490656802d2a6de66dfd1a
FAMASoon <famasoon@gmail.com>
2022-01-24 21:19:02 +0900

    Add string

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -78,6 +78,9 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
@@ -136,4 +139,16 @@ func (l *Lexer) peekChar() byte {
 	} else {
 		return l.input[l.readPostion]
 	}
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+
+	return l.input[position:l.position]
 }
