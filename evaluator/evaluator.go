7c2460dfd7b50986ac9c261c83d25b78b58db901
FAMASoon <famasoon@gmail.com>
2022-01-24 22:50:17 +0900

    Add left right

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -137,6 +137,8 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 		return nativeBoolean(left != right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
@@ -257,6 +259,16 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 	}
 
 	return result
+}
+
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
