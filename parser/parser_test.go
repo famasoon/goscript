ed4c588fb5aa1b8e6c9235c2894a487048c34e0f
FAMASoon <famasoon@gmail.com>
2022-01-24 21:55:10 +0900

    Add string parse case

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,6 +7,24 @@ import (
 	"testing"
 )
 
+func TestStringLiteralExpression(t testing.T) {
+	input := `"hello world";`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+	literal, ok := stmt.Expression.(*ast.StringLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.StringLiteral. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != "hello world" {
+		t.Errorf("literal.Value not %q. got=%q", "hello world", literal.Value)
+	}
+}
+
 func TestLetStatements(t *testing.T) {
 	input := `
 let x = 5;
