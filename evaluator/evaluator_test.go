d476913fc605cd58bc6c5347899c36398361b562
FAMASoon <famasoon@gmail.com>
2022-01-24 22:36:57 +0900

    Add string connection test case

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -375,3 +375,17 @@ func TestStringLiteral(t *testing.T) {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
