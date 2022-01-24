98441cf7fdb41398aa985a085e4f94914c76382c
FAMASoon <famasoon@gmail.com>
2022-01-24 21:45:13 +0900

    Change ast.go

--- ast/ast.go
+++ ast/ast.go
@@ -120,6 +120,15 @@ func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode()      {}
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+func (sl *StringLiteral) String() string       { return sl.Token.Literal }
+
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
