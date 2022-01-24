273bda03470bd6f26984fef5d054ee53eb732be6
FAMASoon <famasoon@gmail.com>
2022-01-24 22:28:25 +0900

    Add string test case

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -361,3 +361,17 @@ func TestFunctionApplication(t *testing.T) {
 		})
 	}
 }
+
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
