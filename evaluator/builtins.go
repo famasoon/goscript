be6db8e4b56524a2e030c803d32088750f92f9b7
FAMASoon <famasoon@gmail.com>
2022-01-25 18:37:05 +0900

    Add len identifier

---
+++ evaluator/builtins.go
@@ -0,0 +1,11 @@
+package evaluator
+
+import "goscript/object"
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			return NULL
+		},
+	},
+}
