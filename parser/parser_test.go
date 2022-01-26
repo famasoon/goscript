cc342a9d13be4db16520ba5da585cd3282107cc2
FAMASoon <famasoon@gmail.com>
2022-01-26 22:45:57 +0900

    Add array case in parser test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -814,3 +814,46 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 	}
 }
 
+func TestParsingArrayLiterals(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	array, ok := stmt.Expression.(*ast.ArrayLiteral)
+	if !ok {
+		t.Fatalf("exp not ast.ArrayLiteral. got=%T", stmt.Expression)
+	}
+
+	if len(array.Elements) != 3 {
+		t.Fatalf("len(array.Elements) not 3. got=%d", len(array.Elements))
+	}
+
+	err := testIntegerLiteral(t, array.Elements[0], 1)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	err = testIntegerLiteral(t, array.Elements[0], 1)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	err = testIntegerLiteral(t, array.Elements[0], 1)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	err = testInfixExpression(t, array.Elements[1], 2, "*", 2)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+
+	err = testInfixExpression(t, array.Elements[2], 3, "+", 3)
+	if err != nil {
+		t.Errorf("[Error] %v", err)
+	}
+}
