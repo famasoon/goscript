680880a54dc0dc04daa72d3180364aed97e043e8
FAMASoon <famasoon@gmail.com>
2022-02-21 23:45:25 +0900

    Add push func

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -77,4 +77,23 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got=%s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length + 1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+
+			return &object.Array{Elements: newElements}
+		},
+	},
 }
