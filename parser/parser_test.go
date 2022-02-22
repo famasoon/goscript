0f580a6fe6a1c05435bef691d46d21c3a6b56083
FAMASoon <famasoon@gmail.com>
2022-02-22 22:11:02 +0900

    Add empty hash test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -935,3 +935,22 @@ func TestParsingHashLiteralsStringKeys(t *testing.T) {
 		}
 	}
 }
+
+func TestParsingEmptyHashLiteral(t *testing.T) {
+	input := "{}"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+	hash, ok := stmt.Expression.(*ast.HashLiteral)
+	if !ok {
+		t.Fatalf("exp is not ast.HashLiteral. got=%T", stmt.Expression)
+	}
+
+	if len(hash.Pairs) != 0 {
+		t.Errorf("hash.Pairs has wrong length. got=%d", len(hash.Pairs))
+	}
+}
